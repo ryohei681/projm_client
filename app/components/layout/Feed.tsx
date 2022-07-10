7a7d71da1378c1bf92acd3f7957086e36667cf55
Ryohei Tsunoda <83001827+ryohei681@users.noreply.github.com>
2022-07-10 17:30:24 +0900

    get post data from firestore and show them in the post section

--- app/components/layout/Feed.tsx
+++ app/components/layout/Feed.tsx
@@ -1,14 +1,39 @@
-import React from 'react'
+import { firestore } from 'config/firebaseApp.config'
+import {
+  DocumentData,
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  QuerySnapshot,
+  QueryDocumentSnapshot,
+} from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
 import styles from './Feed.module.css'
+import Post from './Post'
 
 type Props = {}
 
 const Feed = (props: Props) => {
+  const [posts, setPosts] = useState<QueryDocumentSnapshot<DocumentData>[]>([])
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(firestore, 'posts'), orderBy('timestamp', 'desc')),
+        (snapshot: QuerySnapshot<DocumentData>) => {
+          setPosts(snapshot.docs)
+        },
+      ),
+    [],
+  )
   return (
     <div className={styles.feed}>
       <div className={styles.Home}>
         <h2 className={styles.title}>Home</h2>
       </div>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   )
 }
